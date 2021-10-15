92d7275b0a855c9dd1ae76a72ca9e099a7e83feb
Filipe <filipefreire@protonmail.com>
2021-10-15 15:11:15 +0200

    remove: unnecessary useStoreDispatch hook

--- src/app/hooks/index.ts
+++ src/app/hooks/index.ts
@@ -4,5 +4,4 @@ import { StoreModel } from "../store";
 const typedHooks = createTypedHooks<StoreModel>();
 
 export const useStoreActions = typedHooks.useStoreActions;
-export const useStoreDispatch = typedHooks.useStoreDispatch;
 export const useStoreState = typedHooks.useStoreState;
